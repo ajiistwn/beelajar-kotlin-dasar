1ceaa3830eb5237fde364c195695fdbb82c9b724
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-02-16 09:54:19 +0700

    feat: learn comparison operators, booleans and also if else expressions

---
+++ src/ComparisonOprators.kt
@@ -0,0 +1,13 @@
+fun main () {
+    var a = 100
+    var b = 200
+
+    var result1: Boolean = a > b
+    var result2: Boolean = a < b
+
+    println(result1)
+    println(result2)
+
+    println("a" > "b")
+    println("a" != "a")
+}
---
+++ src/ifExpresion.kt
@@ -0,0 +1,24 @@
+import java.awt.Choice
+
+fun main(){
+
+//    val nilai = 74
+//
+//    if(nilai > 75) {
+//        println("Good Job")
+//    } else {
+//        println("Learn Again")
+//    }
+
+    val ChoiceNum = 2
+
+    if (ChoiceNum == 1){
+        println("Fried rice")
+    } else if (ChoiceNum == 2) {
+        println("Meatball")
+    } else if (ChoiceNum == 3){
+        println("Pizza")
+    } else {
+        println("No option")
+    }
+}
